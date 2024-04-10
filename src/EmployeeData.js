a36788164c253e4b3c87ebe013fa0c19f3d4b080
lalwanid998 <lalwanid998@gmail.com>
2024-04-10 21:20:45 +0530

    first commit

---
+++ src/EmployeeData.js
@@ -0,0 +1,20 @@
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName: 'Mark',
+        lastName: 'Devid',
+        age: 20
+    },
+    {
+        id: 2,
+        firstName: 'Virat',
+        lastName: 'Kohli',
+        age: 34
+    },
+    {
+        id: 3,
+        firstName: 'Rohit',
+        lastName: 'Sharma',
+        age: 35
+    }
+]
